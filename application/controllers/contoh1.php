28fc09f4a91297d752b33b7dea8cc806343050a1
Tiemothy <tiemothy2435@gmail.com>
2023-10-06 14:49:53 +0700

    Menambahkan view data serta form matakuliah

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,10 +3,10 @@ class Contoh1 extends CI_Controller
 {
     public function index()
     {
-        echo"<h1>Perkenalan</h1?";
-    echo"Nama Saya Imam Nawawi 
-    Saya tinggal di daerah Ciputat
-    olah raga yang saya sukai adalah
-    Bulutangkis";
+        echo"<h1>Perkenalan</h1>";
+        echo"Nama Saya Tiemothy Massie 
+        Saya tinggal di daerah Ciputat
+        olah raga yang saya sukai adalah
+        Bulutangkis";
     }
 }
